61d33b2438a0495bfe42d548a4d1eb29370774ae
sripadaskishore <sripadaskishore@outlook.com>
2017-04-07 23:59:02 +0800

    Creating Maven Project Structure to commit

---
+++ src/main/java/HelloWorldTest.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Created by sripa on 7/4/2017.
+ */
+
+import org.junit.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Testing Hello World");
+    }
+}
