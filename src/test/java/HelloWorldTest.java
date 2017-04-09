4afd0dd691464692f2546d04ec6a53b29d73df9a
sripadaskishore <sripadaskishore@outlook.com>
2017-04-09 11:49:54 +0800

    Run Junit Suite

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * Created by sripa on 7/4/2017.
+ */
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Testing Hello World from test package");
+        Assert.assertEquals(false, true);
+    }
+}
