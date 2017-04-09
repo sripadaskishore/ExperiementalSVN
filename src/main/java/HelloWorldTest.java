fd590b840a986b79764f1f8876d02619af0d3578
sripadaskishore <sripadaskishore@outlook.com>
2017-04-09 11:15:35 +0800

    Run Junit Suite

--- src/main/java/HelloWorldTest.java
+++ src/main/java/HelloWorldTest.java
@@ -5,11 +5,15 @@
  */
 
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+@RunWith(Suite.class)
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld() {
         System.out.println("Testing Hello World");
+        Assert.assertEquals(false, true);
     }
 }
